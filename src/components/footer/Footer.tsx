ee5d24a3af596bbcdb5311292baca4077abaab86
Felipe Coelho <felipecoelho.ness@gmail.com>
2023-11-24 21:46:19 -0300

    feat: start

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Ness.admin</span>
+      <span>© 2023 Ness Dev Dashboard All rights reserved.</span>
+    </div>
+  );
+};
+
+export default Footer;
