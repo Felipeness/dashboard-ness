dd23bb9effd1e0661f6a13c8004e318e8d80f742
Felipe Coelho <felipecoelho.ness@gmail.com>
2023-11-27 19:08:40 -0300

    feat: create Products, Users table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,76 @@
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
+import { userRows } from "../../data";
+import { useState } from "react";
+import Add from "../../components/add/Add";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return (
+        <img
+          src={params.row.img || "/noavatar.png"}
+          alt=""
+          className="avatar"
+        />
+      );
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    type: "string",
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "createdAt",
+    headerName: "Create At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
 
 const Users = () => {
-  return <div className="users">Users</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={() => setOpen(true)}>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
+    </div>
+  );
 };
 
 export default Users;
